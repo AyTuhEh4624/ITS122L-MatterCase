cac36b964a9c5d394e6fa47351ae9126a81ed4c6
Jose Andres Ofrin <joseofrin4624@gmail.com>
2025-03-13 13:00:40 +0800

    File Organization

--- dashboard_admin.php
+++ dashboard_admin.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <h1>Admin Dashboard</h1>
-    <a href="viewusers.php" class="button">View Users</a>
+    <a href="view_users.php" class="button">View Users</a>
     <a href="auditlog_page.php" class="button">View Audit Log</a>
     
 </body>

4c6c2d34eadcc39597e035aacac0b298bae0c32a
Jose Andres Ofrin <joseofrin4624@gmail.com>
2025-03-14 16:36:21 +0800

    separated login_page and login function

--- Functions/login.php
+++ Functions/login.php
@@ -87,20 +87,3 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     mysqli_close($conn);
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>  
-    <title>Login</title>
-</head>
-<body>
-    <h1>Login Now</h1>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-        <label>Email:</label>
-        <input type="text" name="username"><br><br>
-        <label>Password:</label>
-        <input type="password" name="pass"><br><br>
-        <input type="submit" value="Login"><br><br>
-    </form>
-</body>
-</html>
